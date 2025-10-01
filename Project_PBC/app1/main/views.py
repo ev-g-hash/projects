5b5179c201b0f4680a9cd2d541853a51f308a553
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-01 18:29:02 +0400

    начало

---
+++ Project_PBC/app1/main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from goods.models import Categories
+
+
+def index(request):
+
+    categories = Categories.objects.all()
+
+    context = {
+        'title': 'Главная PBigCiti',
+        'content': 'Подарок в большом',
+        'content_1': 'городе',
+        'categories': categories
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'PBigCiti - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Подарок в большом городе'
+    }
+    return render(request, 'main/about.html', context)
+
+
+
+
+
