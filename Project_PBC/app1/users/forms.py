cc5f90d84d87c8a0b8ddfc4b8b11aa477a954785
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-13 02:23:17 +0400

    регистрация пользователя

--- Project_PBC/app1/users/forms.py
+++ Project_PBC/app1/users/forms.py
@@ -1,15 +1,41 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
 
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+    
     username = forms.CharField()
     password = forms.CharField()
 
-    # username = forms.CharField(
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields =(
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+    #     
+    #     username = forms.CharField(
     #     widget=forms.TextInput(attrs={"autofocus": True,
     #                                 'class': 'form-control',
     #                                 'placeholder': 'Введите ваше имя пользователя'})
@@ -20,7 +46,6 @@ class UserLoginForm(AuthenticationForm):
     #                                       'class': 'form-control',
     #                                       'placeholder': 'Введите ваш пароль'})
     # )
+
+
     
-    class Meta:
-        model = User
-        fields = ['username', 'password']
