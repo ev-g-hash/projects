33fe1261f2daeb9c934fab789a46fac97e25bc4c
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-14 01:12:25 +0400

    profile_end

--- Project_PBC/app1/users/forms.py
+++ Project_PBC/app1/users/forms.py
@@ -1,11 +1,11 @@
+from dataclasses import fields
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-
     class Meta:
         model = User
         fields = ['username', 'password']
@@ -13,8 +13,8 @@ class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
 
-class UserRegistrationForm(UserCreationForm):
 
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields =(
@@ -33,7 +33,35 @@ class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField()
     password2 = forms.CharField()
 
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
 
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------------
     #     
     #     username = forms.CharField(
     #     widget=forms.TextInput(attrs={"autofocus": True,
